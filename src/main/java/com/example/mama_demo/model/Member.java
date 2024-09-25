62f490f4113f2902e63d8a1f11c18619fc91d49f
Fabian Valle <ranfys.valle@gmail.com>
2024-09-24 23:18:36 -0400

    Update Member.java

--- src/main/java/com/example/mama_demo/model/Member.java
+++ src/main/java/com/example/mama_demo/model/Member.java
@@ -4,6 +4,8 @@ package com.example.mama_demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.*;
+
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
@@ -13,10 +15,19 @@ public class Member implements Serializable {
     @Id
     private String id;
 
+    @NotNull
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
 
+    @NotNull
+    @NotEmpty
+    @Email
     private String email;
 
+    @NotNull
+    @Size(min = 10, max = 12)
+    @Digits(fraction = 0, integer = 12)
     private String phoneNumber;
 
     // getters and setters
